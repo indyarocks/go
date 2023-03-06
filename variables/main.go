2cd12421a627e318ed4c3c7674f17cb4785dc091
Chandan Jhunjhunwal <chandan.jhunjhunwal@gmail.com>
2023-03-06 18:33:09 +0530

    number game 0

--- variables/main.go
+++ variables/main.go
@@ -1,11 +1,29 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
 func main() {
-	var firstNumber int
-	firstNumber = 2
+	// var firstNumber = 2
+	// var secondNumber = 5
+	// var subtraction = 7
+	// var answer int
 
-	var secondNumber = 5
+	reader := bufio.NewReader(os.Stdin)
 
-	subtraction := 7
+	// display a welcome/instructions
+	fmt.Println("Guess the Number game")
+	fmt.Println("----------------------")
+	fmt.Println("")
+
+	fmt.Println("Think of a number between 1 and 10 and press ENTER when ready")
+	reader.ReadString('\n')
+
+	// take them through the game
+
+	// give them the answer
 
 }
