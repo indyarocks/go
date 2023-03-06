9d70e942d38d2b86b8be206ec5c43f0252a9f833
Chandan Jhunjhunwal <chandan.jhunjhunwal@gmail.com>
2023-03-06 16:38:03 +0530

    Added Module

--- Hello-world-go/main.go
+++ Hello-world-go/main.go
@@ -1,15 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"myapp/doctor"
+)
 
 func main() {
-	fmt.Println("Hello-world")
-	// var whatToSay string
-	// whatToSay = "This is a variable"
-	whatToSay := "This is another variable."
-	sayHelloWorld(whatToSay)
-}
-
-func sayHelloWorld(whatToSay string) {
+	var whatToSay string
+	whatToSay = doctor.Intro()
 	fmt.Println(whatToSay)
 }
