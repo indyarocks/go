8d5aaf527689c6b59b3028031f53ffc47b0ebff2
Chandan Jhunjhunwal <chandan.jhunjhunwal@gmail.com>
2023-03-06 16:23:25 +0530

    GoLang: Function with argument

--- Hello-world-go/main.go
+++ Hello-world-go/main.go
@@ -4,6 +4,9 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello-world")
-	fmt.Print("This is some text")
-	fmt.Print("This is some more text")
+	sayHelloWorld("This is Chandan")
+}
+
+func sayHelloWorld(whatToSay string) {
+	fmt.Println(whatToSay)
 }
