f75fbf5a479394e35ea80137c618c49ebe7b8818
Chandan Jhunjhunwal <chandan.jhunjhunwal@gmail.com>
2023-03-06 16:52:25 +0530

    Go Loop

--- Hello-world-go/main.go
+++ Hello-world-go/main.go
@@ -1,11 +1,22 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"myapp/doctor"
+	"os"
 )
 
 func main() {
+	reader := bufio.NewReader(os.Stdin)
+
 	whatToSay := doctor.Intro()
+
 	fmt.Println(whatToSay)
+
+	for {
+		userInput, _ := reader.ReadString('\n')
+
+		fmt.Println(userInput)
+	}
 }
