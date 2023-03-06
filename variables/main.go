22ec7f3631b30f118cd59178a88e907c389545ba
Chandan Jhunjhunwal <chandan.jhunjhunwal@gmail.com>
2023-03-06 18:38:10 +0530

    number game 1

--- variables/main.go
+++ variables/main.go
@@ -6,10 +6,12 @@ import (
 	"os"
 )
 
+const prompt = " and press ENTER when ready."
+
 func main() {
-	// var firstNumber = 2
-	// var secondNumber = 5
-	// var subtraction = 7
+	var firstNumber = 2
+	var secondNumber = 5
+	var subtraction = 7
 	// var answer int
 
 	reader := bufio.NewReader(os.Stdin)
@@ -19,11 +21,21 @@ func main() {
 	fmt.Println("----------------------")
 	fmt.Println("")
 
-	fmt.Println("Think of a number between 1 and 10 and press ENTER when ready")
+	fmt.Println("Think of a number between 1 and 10", prompt)
 	reader.ReadString('\n')
 
 	// take them through the game
+	fmt.Println("Multiply your number by", firstNumber, prompt)
+	reader.ReadString('\n')
 
+	fmt.Println("Now multiply the result by", secondNumber, prompt)
+	reader.ReadString('\n')
+
+	fmt.Println("Divide the result by the number you originally thought of", prompt)
+	reader.ReadString(('\n'))
+
+	fmt.Println("Now subtract", subtraction, prompt)
+	reader.ReadString('\n')
 	// give them the answer
 
 }
