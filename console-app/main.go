4602be8b61f34132c9a82b39381ed196dfe8aa00
Chandan Jhunjhunwal <chandan.jhunjhunwal@gmail.com>
2023-03-07 09:56:04 +0530

    Go map

--- console-app/main.go
+++ console-app/main.go
@@ -18,6 +18,14 @@ func main() {
 		_ = keyboard.Close()
 	}()
 
+	coffees := make(map[int]string)
+	coffees[1] = "Cappucino"
+	coffees[2] = "Latte"
+	coffees[3] = "Americano"
+	coffees[4] = "Mocha"
+	coffees[5] = "Macchiato"
+	coffees[6] = "Espresso"
+
 	fmt.Println("MENU")
 	fmt.Println("----")
 	fmt.Println("1 - Cappucino")
@@ -35,14 +43,15 @@ func main() {
 			log.Fatal(err)
 		}
 
-		i, _ := strconv.Atoi(string(char))
-
-		t := fmt.Sprintf("You chose %d", i)
-		fmt.Println(t)
-
 		if char == 'q' || char == 'Q' {
 			break
 		}
+
+		i, _ := strconv.Atoi(string(char))
+
+		t := fmt.Sprintf("You chose %s", coffees[i])
+		fmt.Println(t)
+
 	}
 
 	fmt.Println("Program existing ....")
