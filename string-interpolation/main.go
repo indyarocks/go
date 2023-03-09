63eca261a611422f677c813a466304316ebfef4a
Chandan Jhunjhunwal <chandan.jhunjhunwal@gmail.com>
2023-03-09 09:25:18 +0530

    string interpolation

---
+++ string-interpolation/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("What is your name?")
+	fmt.Print("-> ")
+
+	userInput, _ := reader.ReadString('\n')
+	userInput = strings.Replace(userInput, "\r\n", "", -1)
+	userInput = strings.Replace(userInput, "\n", "", -1)
+
+	fmt.Println("Your name is", userInput)
+}
