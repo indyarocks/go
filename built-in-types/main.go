f568406d0b4c2065a21e0fba06d848676f2b9ad1
Chandan Jhunjhunwal <chandan.jhunjhunwal@gmail.com>
2023-03-11 22:35:12 +0530

    composite boolean condition using && and ||

--- built-in-types/main.go
+++ built-in-types/main.go
@@ -1,5 +1,34 @@
 package main
 
-func main() {
+import "fmt"
 
+type Employee struct {
+	Name     string
+	Age      int
+	Salary   int
+	FullTime bool
+}
+
+func main() {
+	jack := Employee{
+		Name:     "Jack Smith",
+		Age:      30,
+		Salary:   40000,
+		FullTime: false,
+	}
+
+	john := Employee{
+		Name:     "John Doe",
+		Age:      40,
+		Salary:   500000,
+		FullTime: true,
+	}
+
+	var employees []Employee
+	employees = append(employees, jack)
+	employees = append(employees, john)
+
+	for _, emp := range employees {
+		fmt.Println(emp.Name, emp.Salary, emp.Age, emp.FullTime)
+	}
 }
