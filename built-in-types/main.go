5c8f377c9293e331a3c2fd4b54489f2e776a39cf
Chandan Jhunjhunwal <chandan.jhunjhunwal@gmail.com>
2023-03-11 21:33:15 +0530

    interface

--- built-in-types/main.go
+++ built-in-types/main.go
@@ -1,5 +1,18 @@
 package main
 
+type Dog struct {
+	Name         string
+	Sound        string
+	NumberOfLegs int
+}
+
+type Cat struct {
+	Name         string
+	Sound        string
+	NumberOfLegs int
+	HasTail      bool
+}
+
 func main() {
 
 }
