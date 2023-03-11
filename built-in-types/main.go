69f3f74d0b3b29533e7c5d22658ec78dab1dc84b
Chandan Jhunjhunwal <chandan.jhunjhunwal@gmail.com>
2023-03-11 15:29:40 +0530

    func with pointer argument

--- built-in-types/main.go
+++ built-in-types/main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	x := 10
+
+	myFirstPointer := &x
+
+	fmt.Println("x is", x)
+	fmt.Println("myFirstPointer is", myFirstPointer)
+
+	*myFirstPointer = 15
+	fmt.Println("x is now", x)
+
+	changeValueOfPointer(myFirstPointer)
+	fmt.Println("After function call x is now", x)
+}
+
+func changeValueOfPointer(num *int) {
+	*num = 25
 }
