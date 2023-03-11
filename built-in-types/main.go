1a6ddcd16590f89cc7eb4078c95757c53ff11382
Chandan Jhunjhunwal <chandan.jhunjhunwal@gmail.com>
2023-03-11 18:13:17 +0530

    variatic function and receiver function on user type objects

--- built-in-types/main.go
+++ built-in-types/main.go
@@ -10,7 +10,40 @@ import (
 
 var reader *bufio.Reader
 
+type Animal struct {
+	Name         string
+	Sound        string
+	NumberOfLegs int
+}
+
+// Says - Receiving function
+func (a *Animal) Says() {
+	fmt.Printf("A %s says %s\n", a.Name, a.Sound)
+}
+
+func (a *Animal) HowManyLegs() {
+	fmt.Printf("A %s has %d legs\n", a.Name, a.NumberOfLegs)
+}
+
 func main() {
+	cat := Animal{
+		Name:         "Cat",
+		Sound:        "Meow",
+		NumberOfLegs: 4,
+	}
+
+	cat.Says()
+	cat.HowManyLegs()
+
+	dog := Animal{
+		Name:         "Dog",
+		Sound:        "Woof",
+		NumberOfLegs: 4,
+	}
+
+	dog.Says()
+	dog.HowManyLegs()
+
 	z := addTwoNumbers(2, 4)
 	fmt.Println(z)
 	numbers := getNumbers("Please enter number you want to add. Press any other key to end.")
@@ -40,7 +73,7 @@ func getNumbers(s string) []int {
 	fmt.Println(s)
 	fmt.Println("==>")
 	var numbers []int
-	reader := bufio.NewReader(os.Stdin)
+	reader = bufio.NewReader(os.Stdin)
 	for {
 		userInput, _ := reader.ReadString('\n')
 		userInput = strings.Replace(userInput, "\r\n", "", -1)
