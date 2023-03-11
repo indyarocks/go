2f3dbfc98883b509ebcd2bbdd68883996122269b
Chandan Jhunjhunwal <chandan.jhunjhunwal@gmail.com>
2023-03-11 13:42:58 +0530

    log int

---
+++ built-in-types/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "log"
+
+var myInt int
+var myUint uint
+
+// var myInt16 int16
+// var myInt32 int32
+// var myInt64 int64
+
+var myFloat float32
+var myFloat64 float64
+
+func main() {
+	myInt = 10
+	myUint = 100
+
+	myFloat = 10.1
+	myFloat64 = 100.1
+
+	log.Println(myInt, myUint, myFloat, myFloat64)
+}
