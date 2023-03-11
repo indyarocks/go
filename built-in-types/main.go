18d02c3ceccdaaa034b6981d7a836699f6d8ea29
Chandan Jhunjhunwal <chandan.jhunjhunwal@gmail.com>
2023-03-11 18:48:59 +0530

    Channel routine integration

--- built-in-types/main.go
+++ built-in-types/main.go
@@ -1,8 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/eiannone/keyboard"
+)
+
+var keyPressChan chan rune
 
 func main() {
+	keyPressChan = make(chan rune)
+
+	_ = keyboard.Open()
+	defer func() {
+		_ = keyboard.Close()
+	}()
+
+	fmt.Println("Press any key, or q to exit.")
+	go listenForKeyPress()
+
+	for {
+		char, _, _ := keyboard.GetSingleKey()
+		if char == 'q' || char == 'Q' {
+			fmt.Println("Since you pressed Q, bye bye..")
+			break
+		}
+		keyPressChan <- char
+	}
+
 	go doSomethingInRoutine("I'm inside do something routine")
 
 	fmt.Println("This is first another message")
@@ -11,8 +36,15 @@ func main() {
 
 	fmt.Println("This is second another message")
 
+	// for {
+	// 	// Do nothing
+	// }
+}
+
+func listenForKeyPress() {
 	for {
-		// Do nothing
+		key := <-keyPressChan
+		fmt.Println("You preassed", string(key))
 	}
 }
 
