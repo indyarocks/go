1c739d58c716c03cd6dbc8bcb9418bf807bb62af
Chandan Jhunjhunwal <chandan.jhunjhunwal@gmail.com>
2023-03-11 18:31:09 +0530

    Go routine

--- built-in-types/main.go
+++ built-in-types/main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	go doSomethingInRoutine("I'm inside do something routine")
+
+	fmt.Println("This is first another message")
+
+	go doSomethingInRoutine("I'm inside another something routine")
+
+	fmt.Println("This is second another message")
+
+	for {
+		// Do nothing
+	}
+}
+
+func doSomethingInRoutine(s string) {
+	for i := 0; i < 5; i++ {
+		fmt.Println(s)
+	}
 }
