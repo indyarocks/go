4e0e2977e5ccc73f20e760d1c53c5343b18e339b
Chandan Jhunjhunwal <chandan.jhunjhunwal@gmail.com>
2023-03-11 22:11:16 +0530

    expressions

--- built-in-types/main.go
+++ built-in-types/main.go
@@ -1,5 +1,20 @@
 package main
 
+import "fmt"
+
 func main() {
+	age := 10
+	name := "Jack"
+	rightHanded := true
+
+	fmt.Printf("%s is %d years old. Right handed: %t\n", name, age, rightHanded)
+
+	ageInTenYears := age + 10
+
+	fmt.Printf("In 10 years, %s will be %d years old\n", name, ageInTenYears)
+
+	isATeenager := age >= 13
+
+	fmt.Println(name, "is a teenager:", isATeenager)
 
 }
