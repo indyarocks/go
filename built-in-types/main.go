b1c4ea0dc81b13dba9bf9284a838fc48433564ee
Chandan Jhunjhunwal <chandan.jhunjhunwal@gmail.com>
2023-03-11 21:46:13 +0530

    interface defining

--- built-in-types/main.go
+++ built-in-types/main.go
@@ -1,9 +1,25 @@
 package main
 
+import "fmt"
+
+// Interface
+type Animal interface {
+	Says() string
+	HowManyLegs() int
+}
+
 type Dog struct {
 	Name         string
 	Sound        string
 	NumberOfLegs int
+}
+
+func (d *Dog) Says() string {
+	return d.Sound
+}
+
+func (d *Dog) HowManyLegs() int {
+	return d.NumberOfLegs
 }
 
 type Cat struct {
@@ -13,6 +29,33 @@ type Cat struct {
 	HasTail      bool
 }
 
-func main() {
+func (c *Cat) Says() string {
+	return c.Sound
+}
 
+func (c *Cat) HowManyLegs() int {
+	return c.NumberOfLegs
+}
+
+func main() {
+	dog := Dog{
+		Name:         "Dog",
+		Sound:        "Woof",
+		NumberOfLegs: 4,
+	}
+
+	cat := Cat{
+		Name:         "Cat",
+		Sound:        "Meow",
+		NumberOfLegs: 4,
+		HasTail:      true,
+	}
+
+	Riddle(&dog)
+	Riddle(&cat)
+}
+
+func Riddle(a Animal) {
+	riddle := fmt.Sprintf(`This animal says "%s" and has "%d" legs. What animal is it?`, a.Says(), a.HowManyLegs())
+	fmt.Println(riddle)
 }
