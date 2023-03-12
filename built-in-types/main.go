bfcd23b113c07452e894f814896ab9d1e76084b3
Chandan Jhunjhunwal <chandan.jhunjhunwal@gmail.com>
2023-03-12 12:36:44 +0530

    Composition

--- built-in-types/main.go
+++ built-in-types/main.go
@@ -1,5 +1,63 @@
 package main
 
-func main() {
+import "fmt"
 
+type Vehicle struct {
+	NumberOfWheels     int
+	NumberOfPassengers int
+}
+
+type Car struct {
+	Make       string
+	Model      string
+	Year       int
+	IsElectric bool
+	IsHybrid   bool
+	Vehicle    Vehicle
+}
+
+func (v *Vehicle) ShowDetails() {
+	fmt.Println("Number of passengers:", v.NumberOfPassengers)
+	fmt.Println("Number of wheels:", v.NumberOfWheels)
+}
+
+func (c *Car) Show() {
+	fmt.Println("Make: ", c.Make)
+	fmt.Println("Model: ", c.Model)
+	fmt.Println("Year: ", c.Year)
+	fmt.Println("IsElectric: ", c.IsElectric)
+	fmt.Println("IsHybrid: ", c.IsHybrid)
+	c.Vehicle.ShowDetails()
+}
+
+func main() {
+	suv := Vehicle{
+		NumberOfWheels:     4,
+		NumberOfPassengers: 6,
+	}
+
+	volvoXC90 := Car{
+		Make:       "Volvo",
+		Model:      "XC90 T8",
+		Year:       2021,
+		IsElectric: false,
+		IsHybrid:   true,
+		Vehicle:    suv,
+	}
+
+	volvoXC90.Show()
+	fmt.Println()
+
+	teslaModelX := Car{
+		Make:       "Tesla",
+		Model:      "Model X",
+		Year:       2021,
+		IsElectric: true,
+		IsHybrid:   false,
+		Vehicle:    suv,
+	}
+
+	teslaModelX.Vehicle.NumberOfPassengers = 7
+
+	teslaModelX.Show()
 }
