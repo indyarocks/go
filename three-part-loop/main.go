02340f9fc97186ec86b79339e3d18991d09efcac
Chandan Jhunjhunwal <chandan.jhunjhunwal@gmail.com>
2023-03-12 16:30:33 +0530

    while loop in for

--- three-part-loop/main.go
+++ three-part-loop/main.go
@@ -1,9 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
 func main() {
 	for i := 0; i < 10; i++ {
 		fmt.Println("Looping with index", i)
 	}
+
+	var x int = 1000
+
+	rand.Seed(time.Now().UnixNano())
+
+	for x > 100 {
+		x = rand.Intn(1000) + 1
+		fmt.Println("Value of x is:", x, "and loop continues..")
+	}
+
+	fmt.Println("Got", x, "and broke us out of loop.")
+
 }
