bf04df09f670836eeda0b74750eb2c5af1b37aa9
Chandan Jhunjhunwal <chandan.jhunjhunwal@gmail.com>
2023-03-16 09:04:20 +0530

    Three rounds of game

--- rock-paper-scissors/main.go
+++ rock-paper-scissors/main.go
@@ -16,73 +16,103 @@ const (
 	SCISSORS = 2
 )
 
+var reader *bufio.Reader
+
+func getUserInput() string {
+	reader = bufio.NewReader(os.Stdin)
+	playerChoice, _ := reader.ReadString('\n')
+	playerChoice = strings.Replace(playerChoice, "\r\n", "", -1)
+	playerChoice = strings.Replace(playerChoice, "\n", "", -1)
+	return strings.ToLower(playerChoice)
+}
+
+func getPlayerValue(playerChoice string) int {
+	playerValue := -1
+	if playerChoice == "rock" {
+		playerValue = ROCK
+	} else if playerChoice == "paper" {
+		playerValue = PAPER
+	} else if playerChoice == "scissors" {
+		playerValue = SCISSORS
+	}
+	return playerValue
+}
+
 func main() {
 	playerChoice := ""
 	playerValue := -1
-
-	computerChoice := rand.Intn(3)
+	playerWins := 0
+	computerWins := 0
 
 	clearScreen()
-
-	fmt.Print("Please enter your choice -> ")
-	reader := bufio.NewReader(os.Stdin)
-	playerChoice, _ = reader.ReadString('\n')
-	playerChoice = strings.Replace(playerChoice, "\r\n", "", -1)
-	playerChoice = strings.Replace(playerChoice, "\n", "", -1)
-	lowerPlayerChoice := strings.ToLower(playerChoice)
-
-	fmt.Println()
-	if lowerPlayerChoice == "rock" {
-		playerValue = ROCK
-	} else if lowerPlayerChoice == "paper" {
-		playerValue = PAPER
-	} else if lowerPlayerChoice == "scissors" {
-		playerValue = SCISSORS
-	}
-
-	switch computerChoice {
-	case ROCK:
-		fmt.Println("Computer choose: ROCK")
-		break
-	case PAPER:
-		fmt.Println("Computer choose: PAPER")
-		break
-	case SCISSORS:
-		fmt.Println("Computer choose: SCISSORS")
-		break
-	default:
-	}
-	fmt.Println("You choose: ", playerChoice, "Your value: ", playerValue)
-
-	if playerValue == computerChoice {
-		fmt.Println("It's a draw!")
-	} else {
-		switch playerValue {
+	fmt.Println("Let the game begin. Best of 3 wins!")
+	fmt.Println("===================================")
+	for round := 1; round <= 3; round++ {
+		fmt.Println("Round:", round)
+		fmt.Println("---------------------------------")
+		fmt.Print("Please enter your choice -> ")
+		playerChoice = getUserInput()
+		fmt.Println()
+		playerValue = getPlayerValue(playerChoice)
+		computerValue := rand.Intn(3)
+		switch computerValue {
 		case ROCK:
-			if computerChoice == PAPER {
-				fmt.Println("Computer wins!")
-			} else {
-				fmt.Println("Player wins!")
-			}
+			fmt.Println("Computer choose: ROCK")
 			break
 		case PAPER:
-			if computerChoice == SCISSORS {
-				fmt.Println("Computer wins!")
-			} else {
-				fmt.Println("Player wins!")
-			}
+			fmt.Println("Computer choose: PAPER")
 			break
 		case SCISSORS:
-			if computerChoice == ROCK {
-				fmt.Println("Computer wins!")
-			} else {
-				fmt.Println("Player wins!")
-			}
+			fmt.Println("Computer choose: SCISSORS")
 			break
 		default:
-			fmt.Println("Invalid choice")
 		}
+		fmt.Println("You choose: ", strings.ToUpper(playerChoice))
+
+		if playerValue == computerValue {
+			fmt.Println("It's a draw!")
+			round--
+		} else {
+			switch playerValue {
+			case ROCK:
+				if computerValue == PAPER {
+					computerWins += 1
+					fmt.Println("Computer wins!")
+				} else {
+					playerWins += 1
+					fmt.Println("Player wins!")
+				}
+				break
+			case PAPER:
+				if computerValue == SCISSORS {
+					computerWins += 1
+					fmt.Println("Computer wins!")
+				} else {
+					playerWins += 1
+					fmt.Println("Player wins!")
+				}
+				break
+			case SCISSORS:
+				if computerValue == ROCK {
+					computerWins += 1
+					fmt.Println("Computer wins!")
+				} else {
+					playerWins += 1
+					fmt.Println("Player wins!")
+				}
+				break
+			default:
+				round--
+				fmt.Println("Invalid choice")
+			}
+		}
+		fmt.Println("---------------------------------")
+		fmt.Println()
 	}
+	fmt.Println("Game over!")
+	fmt.Println("Here is the result:")
+	fmt.Printf("Computer wins %d/3\n", computerWins)
+	fmt.Printf("Player wins %d/3\n", playerWins)
 }
 
 func clearScreen() {
