550c8e6a7b3d0cae22b6e176c28d8ad306c22b49
Chandan Jhunjhunwal <chandan.jhunjhunwal@gmail.com>
2023-03-17 23:56:18 +0530

    rock-paper-scissors package

---
+++ rock-paper-scissors-web/rps/rps.go
@@ -0,0 +1,48 @@
+package rps
+
+import (
+	"math/rand"
+)
+
+const (
+	ROCK         = 0 // beats scissors. (scissors + 1) % 3 =0
+	PAPER        = 1 // beats rock. (rock + 1) % 3 = 1
+	SCISSORS     = 2 // beats paper. (paper + 1) % 3 = 2
+	PLAYERWINS   = 1
+	COMPUTERWINS = 2
+	DRAW         = 3
+)
+
+// PlayRound - Arguments - Current Round winner, Computer Choice, Result
+func PlayRound(playerValue int) (int, string, string) {
+	computerValue := rand.Intn(3)
+	computerChoice := ""
+	roundResult := ""
+	winner := 0
+
+	switch computerValue {
+	case ROCK:
+		computerChoice = "Computer chose ROCK"
+		break
+	case PAPER:
+		computerChoice = "Computer chose PAPER"
+		break
+	case SCISSORS:
+		computerChoice = "Computer chose SCISSORS"
+		break
+	default:
+	}
+
+	if playerValue == computerValue {
+		roundResult = "It's a Draw!"
+		winner = DRAW
+	} else if playerValue == (computerValue+1)%3 {
+		roundResult = "Player Wins!"
+		winner = PLAYERWINS
+	} else {
+		roundResult = "Computer Wins!"
+		winner = COMPUTERWINS
+	}
+
+	return winner, computerChoice, roundResult
+}
