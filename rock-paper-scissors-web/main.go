12512f96c5f61bec6f258bcfeae457fcc786046e
Chandan Jhunjhunwal <chandan.jhunjhunwal@gmail.com>
2023-03-17 23:28:22 +0530

    renderTemplate function

--- rock-paper-scissors-web/main.go
+++ rock-paper-scissors-web/main.go
@@ -1,19 +1,31 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 )
 
 func homePage(w http.ResponseWriter, r *http.Request) {
-	html := `<strong>Hello, Chandan</strong>`
-	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprintf(w, html)
+	renderTemplate(w, "index.html")
 }
 
 func main() {
 	http.HandleFunc("/", homePage)
 	log.Println("Starting web server on port 8080...")
 	http.ListenAndServe(":8080", nil)
+}
+
+func renderTemplate(w http.ResponseWriter, page string) {
+	t, err := template.ParseFiles(page)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	err = t.Execute(w, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
 }
