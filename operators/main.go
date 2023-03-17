26bcc12fcc08d6b2f1958723f68c5f3eb55413d7
Chandan Jhunjhunwal <chandan.jhunjhunwal@gmail.com>
2023-03-17 21:02:26 +0530

    errors type

--- operators/main.go
+++ operators/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"math"
 )
@@ -18,4 +19,21 @@ func main() {
 
 	goodThreeSquared := math.Pow(3.0, 2.0)
 	fmt.Println("goodThreeSquared", goodThreeSquared)
+	x := 10
+	y := 0
+
+	c, err := divideTwoNumbers(x, y)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(c)
+	}
+}
+
+func divideTwoNumbers(x, y int) (int, error) {
+	if y == 0 {
+		return 0, errors.New("can't divide by 0")
+	} else {
+		return x / y, nil
+	}
 }
