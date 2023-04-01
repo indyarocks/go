57dfb99445a79c8c996c1c5fc73f32fdeac02b0a
Chandan Jhunjhunwal <chandan.jhun@gmail.com>
2023-04-01 23:39:01 +0530

    Slices

--- built-in-types/main.go
+++ built-in-types/main.go
@@ -1,5 +1,21 @@
 package main
 
+import "fmt"
+
 func main() {
+	fmt.Println("Hello, 世界")
+
+	var mySlice = make([]string, 3)
+	fmt.Println("mySlice", mySlice)
+
+	mySlice[0] = "Chandan"
+	mySlice[1] = "Alok"
+	mySlice[2] = "Ruby"
+	fmt.Println("mySlice", mySlice)
+
+	newSlice := make([]string, len(mySlice))
+	fmt.Println("newSlice", newSlice)
+	copy(newSlice, mySlice)
+	fmt.Println("newSlice", newSlice)
 
 }
