e3b715444d687022cf0034723c39976cb8a4d296
Chandan Jhunjhunwal <chandan.jhun@gmail.com>
2023-04-02 19:33:46 +0530

    Added request counter

--- gopl/web-server/main.go
+++ gopl/web-server/main.go
@@ -4,13 +4,27 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"sync"
 )
 
+var count int
+var mu sync.Mutex
+
 func homePage(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	count++
+	mu.Unlock()
 	fmt.Fprintf(w, "URL Path: %q", r.URL)
+}
+
+func counter(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	fmt.Fprintf(w, "Request Count: %d\n", count)
+	mu.Unlock()
 }
 
 func main() {
 	http.HandleFunc("/", homePage)
+	http.HandleFunc("/count", counter)
 	log.Fatal(http.ListenAndServe("localhost:8080", nil))
 }
