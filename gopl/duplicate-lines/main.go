f208b1721d9a276fda94621c36e2f71788bc50c4
Chandan Jhunjhunwal <chandan.jhun@gmail.com>
2023-04-02 09:15:44 +0530

    Read all input argument files and print the count of occurence of duplicate lines across files

--- gopl/duplicate-lines/main.go
+++ gopl/duplicate-lines/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 )
@@ -9,15 +10,33 @@ func main() {
 	fmt.Println("Calling dup1()....")
 	dup1()
 	fmt.Println("Ending dup1()....")
+	fmt.Println("Args[1:]", os.Args[1:])
 	for _, arg := range os.Args[1:] {
 		fmt.Printf("Argument: %s | %q\t Type: %T\n", arg, arg, arg)
 	}
 	fmt.Println("Calling dup2()....")
-	dup2()
+	dup2(os.Args[1:])
 	fmt.Println("Ending dup2()....")
 }
 
-func dup2() {
+func dup2(args []string) {
+	fmt.Println(args)
+	counts := make(map[string]int)
+	if len(args) == 0 {
+		countLines(os.Stdin, counts)
+	} else {
+		for _, fileName := range args {
+			file, err := os.Open(fileName)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "dups: %v\n", err)
+			}
+			countLines(file, counts)
+			file.Close()
+		}
+		for line, count := range counts {
+			fmt.Printf("%d \t %s\n", count, line)
+		}
+	}
 
 }
 
@@ -35,5 +54,12 @@ func dup1() {
 		if count > 1 {
 			fmt.Printf("%d\t%s\t%T\t%T\n", count, line, count, line)
 		}
+	}
+}
+
+func countLines(file *os.File, counts map[string]int) {
+	input := bufio.NewScanner(file)
+	for input.Scan() {
+		counts[input.Text()]++
 	}
 }
