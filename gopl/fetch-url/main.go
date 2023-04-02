76999e0bcafc6eff2f7295b88977528d22d0fcc5
Chandan Jhunjhunwal <chandan.jhun@gmail.com>
2023-04-02 14:29:58 +0530

    Adding http:// if missing in argument

--- gopl/fetch-url/main.go
+++ gopl/fetch-url/main.go
@@ -6,11 +6,15 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 )
 
 func main() {
 	//	Read arguments os.Args[1:]
 	for _, url := range os.Args[1:] {
+		if !(strings.HasPrefix(url, "http://")) {
+			url = "http://" + url
+		}
 		resp, err := http.Get(url)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "Error: %v\n", err)
