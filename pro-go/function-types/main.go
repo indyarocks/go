73ed38629311294b4ecb39379f0fa0a8ed98633d
Chandan Jhunjhunwal <chandan.jhun@coupa.com>
2023-05-29 18:14:09 +0530

    function types anonymous function and closure

--- pro-go/function-types/main.go
+++ pro-go/function-types/main.go
@@ -2,24 +2,51 @@ package main
 
 import "fmt"
 
-func calcWithTax(price float64) float64 {
-	return price * 0.1
+type calcFunc func(float64) float64
+
+type Product struct {
+	name  string
+	price float64
 }
 
-func calcWithoutTax(price float64) float64 {
-	return 0
+var giveAwayGift = false
+
+//func calcWithTax(price float64) float64 {
+//	return price * 0.1
+//}
+//
+//func calcWithoutTax(price float64) float64 {
+//	return 0
+//}
+
+func printPrice(product string, price float64, priceCalc calcFunc) {
+	fmt.Println("====Product", product, "Final Price", priceCalc(price))
 }
 
-func printPrice(product string, price float64, taxCalculator func(float642 float64) float64) {
-	finalPrice := price + taxCalculator(price)
-	fmt.Println("====Product", product, "Final Price", finalPrice)
-}
-
-func selectCalculator(price float64) func(float64) float64 {
+func selectCalculator(price float64) calcFunc {
 	if price > 300 {
-		return calcWithTax
+		//return calcWithTax
+		return func(price float64) float64 {
+			return price * 0.1
+		}
 	} else {
-		return calcWithoutTax
+		//return calcWithoutTax
+		return func(price float64) float64 {
+			return 0
+		}
+	}
+}
+
+func priceCalcFactory(threshold float64, rate float64) calcFunc {
+	fixedPrizeGiveAway := giveAwayGift
+	return func(price float64) float64 {
+		if fixedPrizeGiveAway {
+			return 0
+		} else if price > threshold {
+			return price + (price * rate)
+		} else {
+			return price
+		}
 	}
 }
 
@@ -34,17 +61,43 @@ func main() {
 		var calTaxUsingSelector func(float64) float64 = selectCalculator(price)
 
 		fmt.Println("function calTax assigned", calTax != nil)
-		if price > 300 {
-			calTax = calcWithTax
-		} else {
-			calTax = calcWithoutTax
-		}
+		//if price > 300 {
+		//	calTax = calcWithTax
+		//} else {
+		//	calTax = calcWithoutTax
+		//}
 
-		fmt.Println("function calTax assigned", calTax != nil)
-		tax := calTax(price)
-		fmt.Println("product", product, "Tax amount", tax)
-		printPrice(product, price, calTax)
+		//fmt.Println("function calTax assigned", calTax != nil)
+		//tax := calTax(price)
+		//fmt.Println("product", product, "Tax amount", tax)
+		//printPrice(product, price, calTax)
 		printPrice(product, price, calTaxUsingSelector)
 	}
 
+	waterSportsProducts := map[string]float64{
+		"ball":         100,
+		"safetyJacket": 200,
+		"glass":        50,
+		"cap":          50,
+	}
+	badmintonSportsProducts := map[string]float64{
+		"shuttle": 20,
+		"racket":  200,
+		"net":     150,
+		"shoes":   400,
+	}
+	giveAwayGift = true
+	waterSportCalc := priceCalcFactory(100, 0.2)
+	giveAwayGift = false
+	bandmintonCalc := priceCalcFactory(150, 0.1)
+
+	fmt.Println("====== Water Sports ======")
+	for name, price := range waterSportsProducts {
+		printPrice(name, price, waterSportCalc)
+	}
+	fmt.Println("====== Badminton Sports ======")
+
+	for name, price := range badmintonSportsProducts {
+		printPrice(name, price, bandmintonCalc)
+	}
 }
