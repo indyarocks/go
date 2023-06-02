2a1f9b080a0f9443bd9a034f73b2aaed48c87f3d
Chandan Jhunjhunwal <chandan.jhun@coupa.com>
2023-06-02 20:14:07 +0530

    Sending and receiving via channel

--- pro-go/concurrency/main.go
+++ pro-go/concurrency/main.go
@@ -2,12 +2,11 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
 func main() {
 	fmt.Println("main function started")
 	CalcStoreTotal(Products)
-	time.Sleep(time.Second * 5)
+	//time.Sleep(time.Second * 5)
 	fmt.Println("main function complete")
 }
--- pro-go/concurrency/operations.go
+++ pro-go/concurrency/operations.go
@@ -2,24 +2,28 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
 func CalcStoreTotal(data ProductData) {
 	var storeTotal float64
+	var channel chan float64 = make(chan float64)
 	for category, productGroup := range data {
 		//storeTotal += productGroup.GetCategoryPrice(category)
-		go productGroup.GetCategoryPrice(category)
+		go productGroup.GetCategoryPrice(category, channel)
+	}
+	for i := 0; i < len(data); i++ {
+		storeTotal += <-channel
 	}
 	fmt.Println("Total:", ToCurrency(storeTotal))
 }
 
-func (productGroup ProductGroup) GetCategoryPrice(category string) (total float64) {
+func (productGroup ProductGroup) GetCategoryPrice(category string, channel chan float64) {
+	var total float64
 	for _, product := range productGroup {
 		fmt.Println(category, "product:", product.Name)
 		total = total + product.Price
-		time.Sleep(time.Millisecond * 100)
+		//time.Sleep(time.Millisecond * 100)
 	}
 	fmt.Println(category, "subtotal:", ToCurrency(total))
-	return
+	channel <- total
 }
