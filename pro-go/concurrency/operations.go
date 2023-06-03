c2ab151030a2a483949e323caab29da9599e79b1
Chandan Jhunjhunwal <chandan.jhun@gmail.com>
2023-06-03 15:38:13 +0530

    inspecting channel buffer

--- pro-go/concurrency/operations.go
+++ pro-go/concurrency/operations.go
@@ -7,7 +7,7 @@ import (
 
 func CalcStoreTotal(data ProductData) {
 	var storeTotal float64
-	var channel chan float64 = make(chan float64)
+	var channel chan float64 = make(chan float64, 2)
 	for category, productGroup := range data {
 		fmt.Println("Invoking Goroutine--")
 		//storeTotal += productGroup.GetCategoryPrice(category)
@@ -15,9 +15,10 @@ func CalcStoreTotal(data ProductData) {
 		fmt.Println("Invoked Goroutine--")
 	}
 	for i := 0; i < len(data); i++ {
-		fmt.Println(time.Now(), "Pending receive from channel----")
+		fmt.Println(len(channel), cap(channel))
+		fmt.Println("----Channel read pending", len(channel), "items in buffer, size", cap(channel))
 		storeTotal += <-channel
-		fmt.Println(time.Now(), "Receive complete from channel----")
+		fmt.Println("----Channel read complete", len(channel), "items in buffer, size", cap(channel))
 		time.Sleep(time.Second)
 	}
 	fmt.Println("Total:", ToCurrency(storeTotal))
@@ -26,12 +27,11 @@ func CalcStoreTotal(data ProductData) {
 func (productGroup ProductGroup) GetCategoryPrice(category string, channel chan float64) {
 	var total float64
 	for _, product := range productGroup {
-		fmt.Println("Category:", category, "--product:", product.Name)
+		//fmt.Println("Category:", category, "--product:", product.Name)
 		total = total + product.Price
 		time.Sleep(time.Millisecond * 100)
 	}
-	fmt.Println(time.Now(), category, "Sending to channel ----")
-	fmt.Println(category, "subtotal:", ToCurrency(total))
+	fmt.Println(category, "Channel sending", ToCurrency(total))
 	channel <- total
-	fmt.Println(time.Now(), category, "Sent to channel ----")
+	fmt.Println(category, "Channel send complete")
 }
