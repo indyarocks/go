cfe8d08df3b0ddb9030aab99a9b5a36ab4ad4926
Chandan Jhunjhunwal <chandan.jhun@gmail.com>
2023-06-03 22:12:42 +0530

    Receiving from multiple channels

--- pro-go/concurrency/main.go
+++ pro-go/concurrency/main.go
@@ -12,31 +12,69 @@ func receiveDispatches(dispatchChannel <-chan DispatchNotification) {
 	}
 }
 
+func enumerateProducts(productChannel chan<- *Product) {
+	for _, product := range ProductList[:3] {
+		productChannel <- product
+		time.Sleep(time.Millisecond * 800)
+	}
+	close(productChannel)
+}
+
 func main() {
 	fmt.Println("main function started")
 	CalcStoreTotal(Products)
 	//time.Sleep(time.Second * 5)
 
 	dispatchChannel := make(chan DispatchNotification, 100)
+	productChannel := make(chan *Product, 100)
 
-	var sendOnlyChannel chan<- DispatchNotification = dispatchChannel
+	//var sendOnlyChannel chan<- DispatchNotification = dispatchChannel
 	//var receiveOnlyChannel <-chan DispatchNotification = dispatchChannel
 
-	go DispatchOrder(sendOnlyChannel)
+	//go DispatchOrder(sendOnlyChannel)
+	go DispatchOrder(chan<- DispatchNotification(dispatchChannel))
+	go enumerateProducts(chan<- *Product(productChannel))
+	channelCount := 2
 	for {
 		select {
 		case order, ok := <-dispatchChannel:
 			if ok {
 				fmt.Println("Dispatch to:", order.Customer, ":", order.Quantity, "x", order.Product.Name)
 			} else {
-				fmt.Println("Channel has been closed")
-				goto alldone
+				fmt.Println("dispatchChannel has been closed")
+				channelCount--
+				dispatchChannel = nil
+			}
+		case product, ok := <-productChannel:
+			if ok {
+				fmt.Println("Product:", product.Name, "Category:", product.Category, "Price:", product.Price)
+			} else {
+				fmt.Println("productChannel has been closed")
+				channelCount--
+				productChannel = nil
 			}
 		default:
+			if channelCount == 0 {
+				goto alldone
+			}
 			fmt.Println("-- No message ready to be received")
 			time.Sleep(time.Millisecond * 500)
 		}
 	}
+	//for {
+	//	select {
+	//	case order, ok := <-dispatchChannel:
+	//		if ok {
+	//			fmt.Println("Dispatch to:", order.Customer, ":", order.Quantity, "x", order.Product.Name)
+	//		} else {
+	//			fmt.Println("Channel has been closed")
+	//			goto alldone
+	//		}
+	//	default:
+	//		fmt.Println("-- No message ready to be received")
+	//		time.Sleep(time.Millisecond * 500)
+	//	}
+	//}
 alldone:
 	fmt.Println("All values received")
 	//receiveDispatches(receiveOnlyChannel)
