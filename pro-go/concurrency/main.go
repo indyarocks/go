3a9554d595d0ba30c8a5ddac120cd170902475fe
Chandan Jhunjhunwal <chandan.jhun@gmail.com>
2023-06-03 16:58:24 +0530

    Use buffered channel and close channel

--- pro-go/concurrency/main.go
+++ pro-go/concurrency/main.go
@@ -8,5 +8,16 @@ func main() {
 	fmt.Println("main function started")
 	CalcStoreTotal(Products)
 	//time.Sleep(time.Second * 5)
+
+	dispatchChannel := make(chan DispatchNotification, 100)
+	go DispatchOrder(dispatchChannel)
+	for {
+		if order, open := <-dispatchChannel; open {
+			fmt.Println("Order dispatched:", order.CustomerName, order.Name, order.Quantity)
+		} else {
+			fmt.Println("Channel has been closed", order)
+			break
+		}
+	}
 	fmt.Println("main function complete")
 }
---
+++ pro-go/concurrency/orderdispatch.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+type DispatchNotification struct {
+	CustomerName string
+	*Product
+	Quantity int
+}
+
+var Customers = []string{"Chandan", "Darsh", "Tithi", "Sheenu"}
+
+func DispatchOrder(channel chan DispatchNotification) {
+	var orderCount = rand.Intn(10) + 2
+	fmt.Println("Order Count:", orderCount)
+	for i := 0; i < orderCount; i++ {
+		customer := Customers[rand.Intn(len(Customers)-1)]
+		quantity := rand.Intn(10) + 1
+		channel <- DispatchNotification{
+			CustomerName: customer,
+			Product:      ProductList[rand.Intn(len(ProductList)-1)],
+			Quantity:     quantity,
+		}
+	}
+	close(channel)
+}
