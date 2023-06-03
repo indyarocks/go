7f74f0033361ca824124518c38cda61b9ace4f8e
Chandan Jhunjhunwal <chandan.jhun@gmail.com>
2023-06-04 01:45:39 +0530

    Error start

--- pro-go/error-handling/main.go
+++ pro-go/error-handling/main.go
@@ -6,6 +6,6 @@ func main() {
 	var categories = []string{"Mobile", "Desktop", "Accessories", "Test"}
 
 	for _, category := range categories {
-		fmt.Println("Category:", category, "Total:", slice.TotalCategoryPrice(category))
+		fmt.Println("Category:", category, "Total:", ToCurrency(slice.TotalCategoryPrice(category)))
 	}
 }
