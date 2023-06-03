0d5da63e9eb432233e464eb66b407ea308b41621
Chandan Jhunjhunwal <chandan.jhun@gmail.com>
2023-06-03 17:09:53 +0530

    Enumerating through channel values

--- pro-go/concurrency/main.go
+++ pro-go/concurrency/main.go
@@ -11,13 +11,18 @@ func main() {
 
 	dispatchChannel := make(chan DispatchNotification, 100)
 	go DispatchOrder(dispatchChannel)
-	for {
-		if order, open := <-dispatchChannel; open {
-			fmt.Println("Order dispatched:", order.CustomerName, order.Name, order.Quantity)
-		} else {
-			fmt.Println("Channel has been closed", order)
-			break
-		}
+	//for {
+	//	if order, open := <-dispatchChannel; open {
+	//		fmt.Println("Order dispatched:", order.CustomerName, order.Name, order.Quantity)
+	//	} else {
+	//		fmt.Println("Channel has been closed", order)
+	//		break
+	//	}
+	//}
+	fmt.Println("Enumerate through channel values in for loop")
+	for order := range dispatchChannel {
+		fmt.Println("Dispatch to:", order.CustomerName, ":", order.Quantity, "x", order.Product.Name)
 	}
+	fmt.Println("Channel has been closed")
 	fmt.Println("main function complete")
 }
--- pro-go/concurrency/orderdispatch.go
+++ pro-go/concurrency/orderdispatch.go
@@ -14,7 +14,7 @@ type DispatchNotification struct {
 var Customers = []string{"Chandan", "Darsh", "Tithi", "Sheenu"}
 
 func DispatchOrder(channel chan DispatchNotification) {
-	var orderCount = rand.Intn(10) + 2
+	var orderCount = rand.Intn(20) + 2
 	fmt.Println("Order Count:", orderCount)
 	for i := 0; i < orderCount; i++ {
 		customer := Customers[rand.Intn(len(Customers)-1)]
