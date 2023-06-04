a8b8ea5e1e52136d11c1f9d07d205c5b274e984c
Chandan Jhunjhunwal <chandan.jhun@gmail.com>
2023-06-04 20:28:45 +0530

    Using error interface

--- pro-go/error-handling/main.go
+++ pro-go/error-handling/main.go
@@ -3,9 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	var categories = []string{"Mobile", "Desktop", "Accessories", "Test"}
+	var categories = []string{"Mobile", "Desktop", "Accessories", "Books"}
 
 	for _, category := range categories {
-		fmt.Println("Category:", category, "Total:", ToCurrency(slice.TotalCategoryPrice(category)))
+		if total, err := slice.TotalCategoryPrice(category); err == nil {
+			fmt.Println("Category:", category, "Total:", ToCurrency(total))
+		} else {
+			fmt.Println(err)
+		}
+
 	}
 }
--- pro-go/error-handling/operations.go
+++ pro-go/error-handling/operations.go
@@ -1,10 +1,25 @@
 package main
 
-func (slice ProductSlice) TotalCategoryPrice(category string) (total float64) {
+type CategoryError struct {
+	requestedCategory string
+}
+
+func (category *CategoryError) Error() string {
+	return "ERROR: Category: " + category.requestedCategory + " doesn't exist."
+}
+
+//type ProductCategoryTotal map[string]float64
+
+func (slice ProductSlice) TotalCategoryPrice(category string) (total float64, err *CategoryError) {
+	var categoryHasProduct bool = false
 	for _, p := range slice {
 		if p.Category == category {
 			total += p.Price
+			categoryHasProduct = true
 		}
+	}
+	if !categoryHasProduct {
+		err = &CategoryError{requestedCategory: category}
 	}
 	return
 }
